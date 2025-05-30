bdae0d957bfb9b4f6228d81e436ab79682e6800c
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-29 21:55:22 -0300

    Using CSS

--- user_register/src/app/layout.jsx
+++ user_register/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import "../styles/app.css";  //file to import global .css files
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ user_register/src/app/stylish/page.jsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/Stylish.module.css';
+export default function Stylish(){
+    return (
+        <div>
+            <h1 className={styles.title}>
+               Style using CSS modules
+            </h1>
+        </div>
+    );
+}
