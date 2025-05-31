6f40d978a571c42441700f882290740d9218c07e
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-31 10:44:32 -0300

    Feat: add link to navegate btw the pages

--- user_register/src/app/page.jsx
+++ user_register/src/app/page.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 export default function start(){
     //"<h1>Title</h1>""
     return (
         <div>
-            <h1>Next.js & React Fundamentals</h1>
-            <h2>Let's start!!</h2>
+            <Link href="/stylish">
+                Stylish
+            </Link>
         </div>
     );
 }
--- user_register/src/app/stylish/page.jsx
+++ user_register/src/app/stylish/page.jsx
@@ -1,10 +1,16 @@
 import styles from '../../styles/Stylish.module.css';
+import Link from 'next/link';
+
+//to use local css is necessary import the file and
+//use className or oder tag like this one className={styles.title}
+
 export default function Stylish(){
     return (
         <div>
-            <h1 className={styles.title}>
-               Style using CSS modules
-            </h1>
+            <div className={styles.title}> 
+                <Link href="/">Return</Link>
+               <h1>Style using CSS modules</h1>
+            </div>
         </div>
     );
 }
