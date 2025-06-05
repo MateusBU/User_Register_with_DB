85f7a35fbe69d108b141eb5a139635c669d1d62f
Mateus Urtassum <mateusurtassum@gmail.com>
2025-06-04 21:44:34 -0300

    Table style with tailwindcss

--- user_register/src/components/Table.tsx
+++ user_register/src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props: TableProps){
     function renderHeader(){
         return(
             <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th className="text-left p-4">Code</th>
+                <th className="text-left p-4">Name</th>
+                <th className="text-left p-4">Age</th>
             </tr> 
         )
     }
@@ -19,18 +19,23 @@ export default function Table(props: TableProps){
         //if there is client (not null), map is used
         return props.clients?.map((client, i) =>{
             return(
-                <tr key={client.id}>
-                    <td>{client.id}</td>
-                    <td>{client.name}</td>
-                    <td>{client.age}</td>
+                <tr key={client.id}
+                    className={i % 2 === 0 ? 'bg-purple-100' : 'bg-purple-200'}>
+                    <td className="text-left p-4">{client.id}</td>
+                    <td className="text-left p-4">{client.name}</td>
+                    <td className="text-left p-4">{client.age}</td>
                 </tr>
             )
         });
     }
 
+    //overflow-hidden ensures the contents respect the border-radius:
     return(
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+            text-gray-100
+                bg-gradient-to-r from-purple-500 via-purple-600 to-purple-800  
+            `}>
                 {renderHeader()}
             </thead>
             <tbody>
