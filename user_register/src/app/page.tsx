1082e695b4df4507c27bd3ee7e6e0ad724440229
Mateus Urtassum <mateusurtassum@gmail.com>
2025-06-04 21:25:12 -0300

    Create Table file, with its function and props

--- user_register/src/app/page.tsx
+++ user_register/src/app/page.tsx
@@ -1,7 +1,17 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 
 export default function Home() {
+
+  const clients = [
+    new Client('Ana', 34, '1'),
+    new Client('Mateus', 30, '2'),
+    new Client('Carlos', 47, '3'),
+    new Client('Pedro', 19, '4'),
+  ]
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -9,7 +19,7 @@ export default function Home() {
       text-white
     `}>
       <Layout title="Simple Register">
-        <span>Content</span>
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   );
---
+++ user_register/src/components/Table.tsx
@@ -0,0 +1,41 @@
+import Client from "../core/Client"
+
+interface TableProps{
+    clients: Client[],
+}
+export default function Table(props: TableProps){
+
+    function renderHeader(){
+        return(
+            <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Age</th>
+            </tr> 
+        )
+    }
+
+    function renderData(){
+        //if there is client (not null), map is used
+        return props.clients?.map((client, i) =>{
+            return(
+                <tr key={client.id}>
+                    <td>{client.id}</td>
+                    <td>{client.name}</td>
+                    <td>{client.age}</td>
+                </tr>
+            )
+        });
+    }
+
+    return(
+        <table>
+            <thead>
+                {renderHeader()}
+            </thead>
+            <tbody>
+                {renderData()}
+            </tbody>
+        </table>
+    )
+}
