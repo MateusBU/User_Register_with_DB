6dbd253e8839c0b7953a0d37a974fbda71d8e498
Mateus Urtassum <mateusurtassum@gmail.com>
2025-06-09 20:34:56 -0300

    [Feat]: - Delete and save clients

--- user_register/src/app/page.tsx
+++ user_register/src/app/page.tsx
@@ -1,40 +1,55 @@
 'use client'; 
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Client from "../core/Client";
+import ClientRequest from "../core/ClientRepository";
+import CollectionClient from "../backend/db/CollectionClient";
 
 
+const repository: ClientRequest = new CollectionClient();
 export default function Home() {
 
+
   const [client, setClient] = useState<Client>(Client.empty()); //type Client
+  const [clients, setClients] = useState<Client[]>([]); //type Client
   const [visible, setVisible] = useState<'table' | 'form'>('table'); //type table or form
 
-  const rawClients = [
-    { name: 'Ana', age: 34, id: '1' },
-    { name: 'Mateus', age: 30, id: '2' },
-    { name: 'Carlos', age: 47, id: '3' },
-    { name: 'Pedro', age: 19, id: '4' },
-  ];
+  useEffect(getAll, []);
+  
+  function getAll(){
+    repository.getAll().then(clients => {
+      setClients(clients);
+      setVisible('table');
+    });
+  }
 
-  // Convert to class instances
-  const clients = rawClients.map(c => new Client(c.name, c.age, c.id));
+  // const rawClients = [
+  //   { name: 'Ana', age: 34, id: '1' },
+  //   { name: 'Mateus', age: 30, id: '2' },
+  //   { name: 'Carlos', age: 47, id: '3' },
+  //   { name: 'Pedro', age: 19, id: '4' },
+  // ];
+
+  // // Convert to class instances
+  // const clients = rawClients.map(c => new Client(c.name, c.age, c.id));
 
   function clientSelected(client: Client) {
     setClient(client);
     setVisible('form');
   }
 
-  function clientDeleted(client: Client) {
-    console.log(client);
+  async function clientDeleted(client: Client) {
+    await repository.delete(client);
+    getAll();
   }
 
-  function saveClient(client: Client){
-    console.log(client);
-    setVisible('table');
+  async function saveClient(client: Client){
+    await repository.save(client);
+    getAll();
   }
 
   function newClient(){
